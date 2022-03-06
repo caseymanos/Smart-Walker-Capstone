2524783287ed320fffd7faacdfcf7fa8185bf9ee
Casey Manos <manoscasey@gmail.com>
2022-03-06 09:06:50 -0500

    Add files via upload

--- Runtime_Demonstration_V2.ino
+++ Runtime_Demonstration_V2.ino
@@ -23,7 +23,7 @@
 
  // Motor values
  const int motorPin1 = 4 ;  // the number of the first Vibration Module pin (PWM)
-// const int motorPin2 = 13 ;  // the number of the second Vibration Module pin (PWM)
+ const int motorPin2 = 13 ;  // the number of the second Vibration Module pin (PWM)
  const long motorInterval = 300000; // 5 minute interval
  unsigned long currentMillis = 0;
  unsigned long previousMotorIntervalMillis = 0; 
@@ -35,34 +35,34 @@ byte hdr, data_h, data_l, chksum;
 // Infrared sensor 
 // define left and right sensors for the upper and lower pairs
 #define irUpLeft A0
-//#define irLowLeft A1
-//#define irUpRight A2
-//#define irLowRight A3
+#define irLowLeft A1
+#define irUpRight A2
+#define irLowRight A3
 
 #define model 20150 // for IR motor being used
 
 SharpIR upperLeftIr(irUpLeft, model);
-//SharpIR lowerLeftIr (irLowLeft, model);
-//SharpIR upperRightIr(irUpRight, model);
-//SharpIR  lowerRightIr(irLowRight, model);
+SharpIR lowerLeftIr (irLowLeft, model);
+SharpIR upperRightIr(irUpRight, model);
+SharpIR  lowerRightIr(irLowRight, model);
 
 // ir distances (in cm)
 int upperLeftIrDist;
-//int lowerLeftIrDist;
-//int lowerRightIrDist;
-//int upperRightIrDist;
+int lowerLeftIrDist;
+int lowerRightIrDist;
+int upperRightIrDist;
 // ToF Distance sensor
 
 VL53L1X leftTof;
-//VL53L1X rightTof;
+VL53L1X rightTof;
 
 // left and right Tof distances (in mm)
 uint16_t leftTofDist; 
-//uint16_t rightTofDist;
+uint16_t rightTofDist;
 
 // TF-Mini Plus values
 TFMPlus lidarLeft;         // Create a TFMini Plus object
-//TFMPlus lidarRight;         // Create a TFMini Plus object
+TFMPlus lidarRight;         // Create a TFMini Plus object
 
  void setup() {
   // Vibration Motor Setup
@@ -70,9 +70,9 @@ TFMPlus lidarLeft;         // Create a TFMini Plus object
     previousMotorIntervalMillis = millis();
 
     pinMode(motorPin1, OUTPUT); // Set digital pin as output
-//    pinMode(motorPin2, OUTPUT); // Set digital pin as output
+    pinMode(motorPin2, OUTPUT); // Set digital pin as output
     analogWrite(motorPin1, powerMode*255); // Start with module off
-//    analogWrite(motorPin2, powerMode); // Start with module off
+    analogWrite(motorPin2, powerMode*255); // Start with module off
 
     Serial.begin(115200); // serial monitor
     Serial.println("Arduino Serial setup");
@@ -88,18 +88,18 @@ TFMPlus lidarLeft;         // Create a TFMini Plus object
     delay(20);               // Give port time to initalize
     lidarLeft.begin( &Serial2);   // Initialize device library object and...
                              // pass device serial port to the object.
-//   Serial3.begin( 57600);  // Initialize TFMPLus device serial port.
-//    delay(20);               // Give port time to initalize
-//    lidarRight.begin( &Serial3);   // Initialize device library object and...
-//                             // pass device serial port to the object.                          
+   Serial3.begin( 115200);  // Initialize TFMPLus device serial port.
+    delay(20);               // Give port time to initalize
+    lidarRight.begin( &Serial3);   // Initialize device library object and...
+                             // pass device serial port to the object.                          
     Serial.println("Lidar Setup");
   
    // initalize pins for the ToF sensors
   pinMode(2, OUTPUT);
-//  pinMode(53, OUTPUT);
+  pinMode(53, OUTPUT);
 
   digitalWrite(2, LOW);
-//  digitalWrite(53, LOW);
+  digitalWrite(53, LOW);
 
   // initialize I2C communication
   delay(500);
@@ -107,34 +107,34 @@ TFMPlus lidarLeft;         // Create a TFMini Plus object
 
   pinMode(2, INPUT);
   delay(150);
-  Serial.println("00");
+  Serial.println("Left ToF Setup");
   leftTof.init(true);
 
   Serial.println("01");
   delay(100);
   leftTof.setAddress(0x28);
   leftTof.setTimeout(100);
-  Serial.println("02");
+  Serial.println("Left ToF Initialize");
 
-//  pinMode(53,INPUT);
-//  delay(150);
-//  rightTof.init(true);
-//  Serial.println("03");
-//  delay(100);
-//  rightTof.setAddress(0x31);
-//  rightTof.setTimeout(100);
-//  Serial.println("04");
+  pinMode(53,INPUT);
+  delay(150);
+  rightTof.init(true);
+  Serial.println("Right ToF Setup");
+  delay(100);
+  rightTof.setAddress(0x31);
+  rightTof.setTimeout(100);
+  Serial.println("Right ToF Initialize");
 //  
   leftTof.setDistanceMode(VL53L1X::Long);
   leftTof.setMeasurementTimingBudget(50000);
   leftTof.startContinuous(50); // continuous reading every 50 ms
   
-//
-//  rightTof.setDistanceMode(VL53L1X::Long);
-//  rightTof.setMeasurementTimingBudget(50000);
-//  rightTof.startContinuous(50); // continuous reading every 50 ms
-//
-//  
+
+  rightTof.setDistanceMode(VL53L1X::Long);
+  rightTof.setMeasurementTimingBudget(50000);
+  rightTof.startContinuous(50); // continuous reading every 50 ms
+
+  
   delay(150);
 
   Serial.println("addresses set");
@@ -175,7 +175,7 @@ TFMPlus lidarLeft;         // Create a TFMini Plus object
   if (currentMillis - previousMotorIntervalMillis >= motorInterval){
     powerMode = ! powerMode;
     analogWrite(motorPin1, powerMode*255); 
-//    analogWrite(motorPin2, powerMode);
+    analogWrite(motorPin2, powerMode*255);
     previousMotorIntervalMillis += motorInterval;
   }
  }
@@ -235,39 +235,40 @@ TFMPlus lidarLeft;         // Create a TFMini Plus object
   Serial.print("\tambient: ");
   Serial.print(leftTof.ranging_data.ambient_count_rate_MCPS);
   Serial.println();
-//  //Right Tof sensor call
-//  rightTof.read();
-//  Serial.print("Right ToF range: ");
-//  Serial.print(rightTof.ranging_data.range_mm);
-//  Serial.print("\tstatus: ");
-//  Serial.print(VL53L1X::rangeStatusToString(rightTof.ranging_data.range_status));
-//  Serial.print("\tpeak signal: ");
-//  Serial.print(rightTof.ranging_data.peak_signal_count_rate_MCPS);
-//  Serial.print("\tambient: ");
-//  Serial.print(rightTof.ranging_data.ambient_count_rate_MCPS);
-//  Serial.println();
+  //Right Tof sensor call
+  rightTof.read();
+  Serial.print("Right ToF range: ");
+  Serial.print(rightTof.ranging_data.range_mm);
+  Serial.print("\tstatus: ");
+  Serial.print(VL53L1X::rangeStatusToString(rightTof.ranging_data.range_status));
+  Serial.print("\tpeak signal: ");
+  Serial.print(rightTof.ranging_data.peak_signal_count_rate_MCPS);
+  Serial.print("\tambient: ");
+  Serial.print(rightTof.ranging_data.ambient_count_rate_MCPS);
+  Serial.println();
  }
+ 
  // Infrared Analog call
  void infraredSensorCall() {
   unsigned long delay1=millis();  // takes the time before the loop on the library begins
 
   upperLeftIrDist = upperLeftIr.distance();
-//  lowerLeftIrDist = lowerLeftIr.distance();
-//  upperRightIrDist = upperRightIr.distance();
-//  lowerRightIrDist = lowerRightIr.distance();
+  lowerLeftIrDist = lowerLeftIr.distance();
+  upperRightIrDist = upperRightIr.distance();
+  lowerRightIrDist = lowerRightIr.distance();
 
   Serial.print("upperLeftIR Mean distance: ");  // returns it to the serial monitor
   Serial.println(upperLeftIrDist);
 
-//  Serial.print("lowerLeftIR Mean distance: ");  // returns it to the serial monitor
-//  Serial.println(lowerLeftIrDist);
-//  
-//  Serial.print("upperRightIR Mean distance: ");  // returns it to the serial monitor
-//  Serial.println(upperRightIrDist);
-//  
-//  Serial.print("upperLeftIR Mean distance: ");  // returns it to the serial monitor
-//  Serial.print(upperLeftIrDist);
-  Serial.print(" ; ");  // returns it to the serial monitor
+  Serial.print("lowerLeftIR Mean distance: ");  // returns it to the serial monitor
+  Serial.println(lowerLeftIrDist);
+  
+  Serial.print("upperRightIR Mean distance: ");  // returns it to the serial monitor
+  Serial.println(upperRightIrDist);
+  
+  Serial.print("upperLeftIR Mean distance: ");  // returns it to the serial monitor
+  Serial.print(upperLeftIrDist);
+  Serial.println(" ; ");  // returns it to the serial monitor
   
   
   unsigned long delay2=millis()-delay1;  // the following gives you the time taken to get the measurement
@@ -280,33 +281,28 @@ int16_t lidarLeftDist = 0;    // Distance to object in centimeters
 int16_t lidarLeftFlux = 0;    // Strength or quality of return signal
 int16_t lidarLeftTemp = 0;    // Internal temperature of Lidar sensor chip
 
-//int16_t lidarRightDist = 0;    
-//int16_t lidarRightFlux = 0;    
-//int16_t lidarRightTemp = 0;   
+int16_t lidarRightDist = 0;    
+int16_t lidarRightFlux = 0;    
+int16_t lidarRightTemp = 0;   
 
   if( lidarLeft.getData( lidarLeftDist, lidarLeftFlux, lidarLeftTemp)) // Get data from the device.
       {
         Serial.print( "Dist: ");
         Serial.println(lidarLeftDist); // display distance,
-//        printf( "Flux:%05i ",   lidarLeftFlux);   // display signal strength/quality,
-//        printf( "Temp:%2i%s",  lidarLeftTemp, "C");   // display temperature,
-//        printf( "\r\n");                   // end-of-line.
       }
       else                  // If the command fails...
       {
         lidarLeft.printFrame();  // display the error and HEX dataa
       }
 
-//  if( lidarRight.getData( lidarRightDist, lidarRightFlux, lidarRightTemp)) // Get data from the device.
-//      {
-//        printf( "Dist:%04icm ",  lidarRightDist);   // display distance,
-//        printf( "Flux:%05i ",   lidarRightFlux);   // display signal strength/quality,
-//        printf( "Temp:%2i%s",  lidarRightTemp, "C");   // display temperature,
-//        printf( "\r\n");                   // end-of-line.
-//      }
-//      else                  // If the command fails...
-//      {
-//        lidarRight.printFrame();  // display the error and HEX dataa
-//      }
+  if( lidarRight.getData( lidarRightDist, lidarRightFlux, lidarRightTemp)) // Get data from the device.
+      {
+       Serial.print( "Dist: ");
+        Serial.println(lidarRightDist); // display distance,
+      }
+      else                  // If the command fails...
+      {
+        lidarRight.printFrame();  // display the error and HEX dataa
+      }
  }
  
